fff2c5951356ec46290dd36ba1271e56a55938c3
mariusfelea <mariusfelea23@gmail.com>
2017-03-17 16:10:03 +0200

    second commit

---
+++ src/test/java/BaseControllerTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class BaseControllerTest {
+
+	@Test
+	public void sum() {
+		int first = 1;
+		int second = 2;
+		
+		int sum = 5;
+		
+		assertTrue("Error sum not equal with first + second", sum == first + second);
+	}
+}
--- target/CounterWebApp/WEB-INF/pages/index.jsp
+++ target/CounterWebApp/WEB-INF/pages/index.jsp
@@ -5,4 +5,4 @@
 <h3>Message : ${message}</h3>
 <h3>Counter : ${counter}</h3>	
 </body>
-</html>
+</html> 
